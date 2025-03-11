067383e9463ccdff3eb7611339491a04613ea64b
nandaa11109 <amfernanda0907@gmail.com>
2025-03-11 19:26:12 +0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,7 +8,7 @@ const About = () =>{
                     <div className="max-w-1/2 mx-auto py-8">
                         <h1 className="text-4xl font-bold mb-4 mt-10">About Me</h1>
                         <BlurText
-                        text="As a fresh graduate from Universitas Muhammadiyah Jember, I have an interest in learning and developing in the field of UI/UX both web and mobile design. I also have interests and talents in the field of web developers, namely Frontend Developer with Html, Css, Bootstrap, Tailwind, React, Javascript tools."
+                        text="As a fresh graduate from Muhammadiyah University of Jember, I have an interest in learning and developing in the field of UI/UX both web and mobile design. I also have interests and talents in the field of web developers, namely Frontend Developer with Html, Css, Bootstrap, Tailwind, React, Javascript tools."
                         delay={50}
                         animateBy="words"
                         direction="top"
